f0f8a0a4a30d35a8ad0681137c3a1577a10fa8d3
Marco André Steimbach <marco.steimbach@gmail.com>
2023-06-16 09:19:41 -0300

    Realizado Correçoes

--- src/assets/Sections/CardsFoods/index.jsx
+++ src/assets/Sections/CardsFoods/index.jsx
@@ -1,39 +1,42 @@
 import React, { useState } from 'react'
 import { SectionCardsFood, DivCard, ImgCardFood, DivInformation, NameOfFood, CategoryName, ValueStyled, ButtonStyled } from './styled'
-import { Toastfy } from '../Toastfy'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 export const CardFood = ({ foodList, selectedItems, setSelectedItems }) => {
-    const [showToast, setShowToast] = useState(false)
+  const handleAddItem = (food) => {
+    let itemExists = false
+    selectedItems.forEach((item) => {
+      if (item.id === food.id) {
+        itemExists = true
+      }
+    })
 
-    const handleAddItem = (food) => {
-        let itemExists = false
-        selectedItems.forEach((item) => {
-            if (item.id === food.id) {
-                itemExists = true
-            }
-        })
-        if (itemExists) {
-            setShowToast(true)
-            setTimeout(() => { setShowToast(false) }, 5000)
-            return
-        }
-        setSelectedItems(prevItems => [...prevItems, food])
+    if (itemExists) {
+      toast.error('Item já adicionado', {
+        position: toast.POSITION.TOP_RIGHT,
+        autoClose: 5000,
+      })
+      return
     }
 
-    return (
-        <SectionCardsFood>
-            {foodList.map((food) => (
-                <DivCard key={food.id}>
-                    <ImgCardFood src={food.img}></ImgCardFood>
-                    <DivInformation>
-                        <NameOfFood>{food.name}</NameOfFood>
-                        <CategoryName>{food.category}</CategoryName>
-                        <ValueStyled>R$ {food.price}</ValueStyled>
-                        <ButtonStyled onClick={() => handleAddItem(food)}>Adicionar</ButtonStyled>
-                    </DivInformation>
-                </DivCard>
-            ))}
-            {showToast && <Toastfy />}
-        </SectionCardsFood>
-    )
+    setSelectedItems(prevItems => [...prevItems, food])
+  }
+
+  return (
+    <SectionCardsFood>
+      {foodList.map((food) => (
+        <DivCard key={food.id}>
+          <ImgCardFood src={food.img}></ImgCardFood>
+          <DivInformation>
+            <NameOfFood>{food.name}</NameOfFood>
+            <CategoryName>{food.category}</CategoryName>
+            <ValueStyled>R$ {food.price}</ValueStyled>
+            <ButtonStyled onClick={() => handleAddItem(food)}>Adicionar</ButtonStyled>
+          </DivInformation>
+        </DivCard>
+      ))}
+      <ToastContainer />
+    </SectionCardsFood>
+  )
 }
